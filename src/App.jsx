35e8a314589ac4fa3480eabaf53e64b8dee60819
jonathan briceño <brimanz2014@gmail.com>
2022-11-12 20:54:34 -0400

    optimizing component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import Navbar from './components/navbar/Navbar'
-import Header from './components/header/Header'
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
+import {
+  Navbar, 
+  Header, 
+  About, 
+  Projects, 
+  Contact
+} from './components'
 
 
 const App = () =>{
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from './navbar/Navbar'
+import Header from './header/Header'
+import About from './about/About'
+import Projects from './projects/Projects'
+import Contact from './contact/Contact'
+
+
+export {
+	Navbar, 
+	Header, 
+	About, 
+	Projects, 
+	Contact
+}
